0ee1796e7af375db4b1707dc66637306e038e477
lakshya-bang <kanhazbang16@gmail.com>
2023-08-11 16:22:10 +0530

    Comments added to Beam

--- crs-group-a-wibmo-pos/src/com/wibmo/bean/Grade.java
+++ crs-group-a-wibmo-pos/src/com/wibmo/bean/Grade.java
@@ -12,35 +12,66 @@ public class Grade {
 	int courseID;
 	int studentId;
 	char grade;
-	
+	/**
+	 * @return the year
+	 */
 	public int getYear() {
 		return year;
 	}
+	/**
+	 * @param year the year to set
+	 */
 	public void setYear(int year) {
 		this.year = year;
 	}
+	/**
+	 * @return the sem
+	 */
 	public char getSem() {
 		return sem;
 	}
+	/**
+	 * @param sem the sem to set
+	 */
 	public void setSem(char sem) {
 		this.sem = sem;
 	}
+	/**
+	 * @return the courseID
+	 */
 	public int getCourseID() {
 		return courseID;
 	}
+	/**
+	 * @param courseID the courseID to set
+	 */
 	public void setCourseID(int courseID) {
 		this.courseID = courseID;
 	}
+	/**
+	 * @return the studentId
+	 */
 	public int getStudentId() {
 		return studentId;
 	}
+	/**
+	 * @param studentId the studentId to set
+	 */
 	public void setStudentId(int studentId) {
 		this.studentId = studentId;
 	}
+	/**
+	 * @return the grade
+	 */
 	public char getGrade() {
 		return grade;
 	}
+	/**
+	 * @param grade the grade to set
+	 */
 	public void setGrade(char grade) {
 		this.grade = grade;
 	}
+	
+	
 }
--- crs-group-a-wibmo-pos/src/com/wibmo/bean/Professor.java
+++ crs-group-a-wibmo-pos/src/com/wibmo/bean/Professor.java
@@ -6,23 +6,42 @@ public class Professor {
 	private String professorName;
 	private int professorID; 
 	private List<Course> coursesTaught;
+	/**
+	 * @return the professorName
+	 */
 	public String getProfessorName() {
 		return professorName;
 	}
-	public void setProfessorName(String name) {
-		professorName = name;
+	/**
+	 * @param professorName the professorName to set
+	 */
+	public void setProfessorName(String professorName) {
+		this.professorName = professorName;
 	}
+	/**
+	 * @return the professorID
+	 */
 	public int getProfessorID() {
 		return professorID;
 	}
-	public void setProfessorID(int iD) {
-		professorID = iD;
+	/**
+	 * @param professorID the professorID to set
+	 */
+	public void setProfessorID(int professorID) {
+		this.professorID = professorID;
 	}
+	/**
+	 * @return the coursesTaught
+	 */
 	public List<Course> getCoursesTaught() {
 		return coursesTaught;
 	}
+	/**
+	 * @param coursesTaught the coursesTaught to set
+	 */
 	public void setCoursesTaught(List<Course> coursesTaught) {
 		this.coursesTaught = coursesTaught;
 	}
+	
 	
 }
