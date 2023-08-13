13aa8c7693e94b08bd7ca141d3112ff223bc7c3c
lakshya-bang <kanhazbang16@gmail.com>
2023-08-13 21:32:31 +0530

    User Auth

--- crs-group-a-wibmo-pos/src/com/wibmo/business/AuthenticationService.java
+++ crs-group-a-wibmo-pos/src/com/wibmo/business/AuthenticationService.java
@@ -15,5 +15,4 @@ public interface AuthenticationService {
 	 * TODO: should return a FailureReson (Invalid Password, User does not exist, etc.)
 	 * TODO: should return UserDetails w.r.t. UserType of this user.
 	 */
-	public User login(Long userId, String password);
 }
--- crs-group-a-wibmo-pos/src/com/wibmo/business/FakeAuthenticationService.java
+++ crs-group-a-wibmo-pos/src/com/wibmo/business/FakeAuthenticationService.java
@@ -13,29 +13,29 @@ import com.wibmo.bean.User;
  */
 public class FakeAuthenticationService implements AuthenticationService {
 
-	User user1 = new User("Lakshya", 101L, "Bangalore", "lakshya@test.com", "Student", "abc");
-	User user2 = new User("Lakshya", 102L, "India", "lakshya@test.com", "Professor", "abc");
-	User user3 = new User("Lakshya", 103L, "Delhi", "lakshya@test.com", "Admin", "abc");
-	Map<Long,Map<String,User>> fakeAuthDB = new HashMap<Long,Map<String,User>>();
-	
+	static private User user1 = new User("Lakshya", 101L, "Bangalore", "lakshya1@test.com", "Student", "abc");
+	static private User user2 = new User("Lakshya", 102L, "India", "lakshya2@test.com", "Professor", "abc");
+	static private User user3 = new User("Lakshya", 103L, "Delhi", "lakshya3@test.com", "Admin", "abc");
+	static Map<String,Map<String,User>> fakeAuthDB = new HashMap<>();
 	
 	public FakeAuthenticationService() {
-		fakeAuthDB.put(101L, Map.of("abc",user1));
-		fakeAuthDB.put(102L, Map.of("abc",user2));
-		fakeAuthDB.put(103L, Map.of("abc",user3));
+		
 	}
-	
-	@Override
-	public User login(Long userId, String password) {
-		if(fakeAuthDB.containsKey(userId)) {
+	public static User login(String username, String password) {
+		fakeAuthDB.put(user1.getUserEmail(), Map.of("abc",user1));
+		fakeAuthDB.put(user2.getUserEmail(), Map.of("abc",user2));
+		fakeAuthDB.put(user3.getUserEmail(), Map.of("abc",user3));	
+		
+		if(!fakeAuthDB.containsKey(username)) {
 			// TODO: Should return "User does not exists."
 			return null;
 		}
-		if(!fakeAuthDB.get(userId).containsKey(password)) {
+		
+		if(!fakeAuthDB.get(username).containsKey(password)) {
 			// TODO: Should return "Incorrect password."
 			return null;
 		}
-		return fakeAuthDB.get(userId).get(password);
+		return fakeAuthDB.get(username).get(password);
 	}
 
 }
