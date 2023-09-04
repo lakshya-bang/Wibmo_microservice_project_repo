4e444647d15a4f4d4b84113ccb8893cece853d6d
lakshya-bang <kanhazbang16@gmail.com>
2023-09-04 15:34:46 +0530

    Password Encoder Issue

--- MICROSERVICES/CRS-GROUP-A-WIBMO-AUTHENTICATION-MICROSERVICE/src/main/java/com/wibmo/config/WebSecurityConfig.java
+++ MICROSERVICES/CRS-GROUP-A-WIBMO-AUTHENTICATION-MICROSERVICE/src/main/java/com/wibmo/config/WebSecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -44,6 +45,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		auth.userDetailsService(jwtUserDetailsService);
 	}
 
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+	    return NoOpPasswordEncoder.getInstance();
+	}
 
 	@Bean
 	@Override
