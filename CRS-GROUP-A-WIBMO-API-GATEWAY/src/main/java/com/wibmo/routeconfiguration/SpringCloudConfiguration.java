935429c7db95f63439d1ff0ef901ff2b2059ba61
abhishek-bits <h20211030070@hyderabad.bits-pilani.ac.in>
2023-09-04 09:31:41 +0530

    add api gateway

---
+++ CRS-GROUP-A-WIBMO-API-GATEWAY/src/main/java/com/wibmo/routeconfiguration/SpringCloudConfiguration.java
@@ -0,0 +1,26 @@
+package com.wibmo.routeconfiguration;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class SpringCloudConfiguration {
+
+    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+    	
+    	System.out.println("n router");
+        return builder.routes()
+        		 .route(r -> r
+        				 .path("/api/student/**")
+        				 .uri("http://localhost:8083/"))
+        		 .route(r -> r
+						 .path("/api/professor/**")
+						 .uri("http://localhost:8082/"))
+        		 .build();
+    }
+
+}
