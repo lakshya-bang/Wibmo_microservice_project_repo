80a2f043c22ff5e2b174020cab8a694a00a81091
Mana21li <manalikumari121@gmail.com>
2023-09-04 21:34:16 +0530

    add comments, swagger and documentation

--- MICROSERVICES/CRS-GROUP-A-WIBMO-PAYMENTS-MICROSERVICE/src/main/java/com/wibmo/config/WebSecurityConfig.java
+++ MICROSERVICES/CRS-GROUP-A-WIBMO-PAYMENTS-MICROSERVICE/src/main/java/com/wibmo/config/WebSecurityConfig.java
@@ -60,6 +60,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 		httpSecurity.csrf().disable()
 				// dont authenticate this particular request
 				.authorizeRequests().
+				antMatchers("/v3/api-docs/**", "/swagger-ui/**",
+                        "/v2/api-docs/**", "/swagger-resources/**").permitAll().
 				// all other requests need to be authenticated
 				anyRequest().authenticated().and().
 				// make sure we use stateless session; session won't be used to
--- MICROSERVICES/CRS-GROUP-A-WIBMO-PAYMENTS-MICROSERVICE/src/main/java/com/wibmo/repository/PaymentRepository.java
+++ MICROSERVICES/CRS-GROUP-A-WIBMO-PAYMENTS-MICROSERVICE/src/main/java/com/wibmo/repository/PaymentRepository.java
@@ -13,9 +13,23 @@ import com.wibmo.entity.Payment;
  */
 public interface PaymentRepository extends CrudRepository<Payment, Integer> {
 
+	/**
+	 * This is a repository Method to find Payment using course registration id
+	 * @param courseRegistrationId id of registered course of a student
+	 * @return Optional Payment object or null
+	 */
 	Optional<Payment> findByCourseRegistrationId(Integer courseRegistrationId);
 	
+	/**
+	 * This is a repository Method to find All Payment entries 
+	 * @return List of Payment
+	 */
 	List<Payment> findAll();
 	
+	/**
+	 * This is a repository Method to find All Payment entries given course registraton ids as list
+	 * @param courseRegistrationIds id of registered course of a student
+	 * @return List of Payment
+	 */
 	List<Payment> findAllByCourseRegistrationIdIn(Collection<Integer> courseRegistrationIds);
 }
