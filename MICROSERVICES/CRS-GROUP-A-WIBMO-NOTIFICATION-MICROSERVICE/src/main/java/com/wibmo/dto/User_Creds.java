8b5f91301f2f7318a69a7bf7a8af24307f59a606
lakshya-bang <kanhazbang16@gmail.com>
2023-09-07 11:33:04 +0530

    Authentication in Notification

---
+++ MICROSERVICES/CRS-GROUP-A-WIBMO-NOTIFICATION-MICROSERVICE/src/main/java/com/wibmo/dto/User_Creds.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package com.wibmo.dto;
+
+/**
+ * 
+ */
+public class User_Creds {
+		String userEmail;
+		String password;
+		/**
+		 * @return the userName
+		 */
+		public String getUserEmail() {
+			return userEmail;
+		}
+		/**
+		 * @param userName the userName to set
+		 */
+		public void setUserEmail(String userName) {
+			this.userEmail = userName;
+		}
+		/**
+		 * @return the password
+		 */
+		public String getPassword() {
+			return password;
+		}
+		/**
+		 * @param password the password to set
+		 */
+		public void setPassword(String password) {
+			this.password = password;
+		}
+		
+}
